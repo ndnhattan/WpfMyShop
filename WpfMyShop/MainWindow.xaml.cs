5ed1731d20a33ba974e36b30325063f5dd9c80e8
Quang Trần <buuquang102@gmail.com>
2023-10-20 18:06:24 +0700

    Them login va luu local

--- WpfMyShop/MainWindow.xaml.cs
+++ WpfMyShop/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +27,150 @@ namespace WpfMyShop
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+                textEmail.Visibility = Visibility.Collapsed;
+            else
+                textEmail.Visibility = Visibility.Visible;
+        }
+
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPasswordBox.Focus();
+        }
+
+        private void txtPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPasswordBox.Password) && txtPasswordBox.Password.Length > 0)
+                textPassword.Visibility = Visibility.Collapsed;
+            else
+                textPassword.Visibility = Visibility.Visible;
+        }
+
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private async void logInButton_Click(object sender, RoutedEventArgs e)
+        {
+            //if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(txtPasswordBox.Password))
+            //{
+            //    MessageBox.Show("Successfully Signed In");
+            //}
+
+            // Connect to db
+            string username = txtEmail.Text;
+            string password = txtPasswordBox.Password;
+
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = ".\\SQLSERVER2016"; // tên server demo
+            builder.InitialCatalog = "MyShop"; // tên database demo
+            builder.UserID = username;
+            builder.Password = password;
+            builder.TrustServerCertificate = true;
+
+            string connectionString = builder.ConnectionString;
+            var connection = new SqlConnection(connectionString);
+
+            connection = await Task.Run(() => {
+                var _connection = new SqlConnection(connectionString);
+
+                try
+                {
+                    _connection.Open();
+                }
+                catch (Exception ex)
+                {
+
+                    _connection = null;
+                }
+
+                // Test khi chạy quá nhanh
+                //System.Threading.Thread.Sleep(3000);
+                return _connection;
+            });
+
+            // save username and password if user check Remeber me
+            if (connection != null)
+            {
+                MessageBox.Show(
+                    $"Connected"
+                );
+                if (RememberMe.IsChecked == true)
+                {
+                    var passwordInBytes = Encoding.UTF8.GetBytes(password);
+                    var entropy = new byte[20];
+                    using (var rng = new RNGCryptoServiceProvider())
+                    {
+                        rng.GetBytes(entropy);
+                    }
+                    var cypherText = ProtectedData.Protect(passwordInBytes, entropy,
+                        DataProtectionScope.CurrentUser);
+                    var passwordIn64 = Convert.ToBase64String(cypherText);
+                    var entropyIn64 = Convert.ToBase64String(entropy);
+
+                    var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                    config.AppSettings.Settings["Username"].Value = username;
+                    config.AppSettings.Settings["Password"].Value = passwordIn64;
+                    config.AppSettings.Settings["Entropy"].Value = entropyIn64;
+                    config.Save(ConfigurationSaveMode.Minimal);
+
+                    ConfigurationManager.RefreshSection("appSettings");
+                }
+            }
+            else
+            {
+                // Cannot connect to db
+                MessageBox.Show(
+                    $"Cannot connect"
+                );
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Load username and password
+            var passwordIn64 = ConfigurationManager.AppSettings["Password"];
+            if (passwordIn64.Length != 0)
+            {
+                var entropyIn64 = ConfigurationManager.AppSettings["Entropy"];
+                var cyperTextInBytes = Convert.FromBase64String(passwordIn64);
+                var entropyInBytes = Convert.FromBase64String(entropyIn64);
+                var passwordInBytes = ProtectedData.Unprotect(cyperTextInBytes, entropyInBytes,
+                    DataProtectionScope.CurrentUser);
+                var password = Encoding.UTF8.GetString(passwordInBytes);
+                txtPasswordBox.Password = password;
+                txtEmail.Text = ConfigurationManager.AppSettings["Username"];
+            }
+
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            string bufferImgClose = "assets/close.png";
+            string t_imgClose = $"{baseDirectory}{bufferImgClose}";
+            var bitmapImgClose = new BitmapImage(new Uri(t_imgClose, UriKind.Absolute));
+            imgClose.Source = bitmapImgClose;
+
+            string bufferImgEmail = "assets/email.png";
+            string t_imgEmail = $"{baseDirectory}{bufferImgEmail}";
+            var bitmapImgEmail = new BitmapImage(new Uri(t_imgEmail, UriKind.Absolute));
+            imgEmail.Source = bitmapImgEmail;
+
+            string bufferImgPassword = "assets/password.png";
+            string t_imgPassword = $"{baseDirectory}{bufferImgPassword}";
+            var bitmapImgPassword = new BitmapImage(new Uri(t_imgPassword, UriKind.Absolute));
+            imgPassword.Source = bitmapImgPassword;
+
         }
     }
 }
